b69208387a9f61db04089c1225f0bf8e6760d8d1
Divyansh Singh <57490748+DivyanshSingh12@users.noreply.github.com>
2023-06-09 23:24:30 +0530

    Update Books_j.js
    Adding "EventListeners" for "touchmove" and "touchstart" and then doing the same that we do when  "Right arrow" or "Left arrow" is clicked.

--- Books_j.js
+++ Books_j.js
@@ -5,7 +5,6 @@ function Right1(){
     document.getElementById("tex1").classList.remove("inv");
 }
 
-
 document.querySelector(".Right-arrow").addEventListener("click", Right1)
 document.querySelector(".sv0").addEventListener("click", Right1)
 
@@ -18,3 +17,21 @@ function Left1(){
 
 document.querySelector(".left-arrow").addEventListener("click", Left1)
 document.querySelector(".sv1").addEventListener("click", Left1)
+
+document.querySelector(".slideshow").addEventListener("touchstart", touchStart);
+
+function touchStart(){
+    document.querySelector(".slideshow").addEventListener("touchmove", navigateTo());
+}
+
+function navigateTo(){
+    if(document.getElementById("im").classList.contains("inv")){
+        Left1();
+        return;
+    }
+
+    if(document.getElementById("im1").classList.contains("inv")){
+        Right1();
+        return;
+    }
+}
